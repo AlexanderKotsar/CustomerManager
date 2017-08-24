d743b3e1013dc4a1ab1cfc1912218b6924f1a66c
Alexander Kotsar <kotsandroid@gmail.com>
2017-08-24 17:29:00 +0300

    Add DOCUMENTATION WITH SWAGGER 2

--- src/main/java/xyz/kots/config/SwaggerConfig.java
+++ src/main/java/xyz/kots/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package xyz.kots.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,10 +15,22 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket productApi() {
+    public Docket customerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("xyz.kots.controller"))
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("xyz.kots.controller"))
                 .paths(regex("/customer.*"))
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("Spring Boot REST API")
+                .contact("Alexander Kotsar")
+                .license("Angular 2 front-end App")
+                .licenseUrl("https://customermanagerf.herokuapp.com/")
+                .version("1.0")
                 .build();
     }
 }
