153ece2ec55bfeee7339149d67e263dad6c34aaf
Alexander Kotsar <kotsandroid@gmail.com>
2017-08-25 13:05:44 +0300

    Refactoring

--- src/main/java/xyz/kots/controller/CustomerController.java
+++ src/main/java/xyz/kots/controller/CustomerController.java
@@ -13,49 +13,55 @@ import xyz.kots.service.ICustomerService;
 
 import java.util.List;
 
-@RestController
-@CrossOrigin
-@Api(description="Operations pertaining to customers")
-public class CustomerController {
+    @RestController
+    @CrossOrigin
+    @Api(description="Operations pertaining to customers")
+    public class CustomerController {
 
-    @Autowired
-    private ICustomerService customerService;
+        @Autowired
+        private ICustomerService customerService;
 
-    @ApiOperation(value = "Search a customer with an ID")
-    @GetMapping("customer")
-    public ResponseEntity<Customer> getCustomerById(@RequestParam("id") String id) {
-        Customer customer= customerService.getCustomerId(Integer.parseInt(id));
-        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        @ApiOperation(value = "Search a customer with an ID")
+        @GetMapping("customer")
+        public ResponseEntity<Customer> getCustomerById(@RequestParam("id") String id) {
+            Customer customer = customerService.getCustomerId(Long.parseLong(id));
+            if (customer == null)
+                return new ResponseEntity(HttpStatus.GONE);
+            return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        }
+
+        @ApiOperation(value = "Search a customer with an NAME")
+        @GetMapping("customers/{name}")
+        public ResponseEntity<List<Customer>> getCustomerByName(@PathVariable("name") String name) {
+            List<Customer> listCustomers = customerService.getCustomerName(name);
+            if (listCustomers.isEmpty())
+                return new ResponseEntity(HttpStatus.GONE);
+            return new ResponseEntity<List<Customer>>(listCustomers, HttpStatus.OK);
+
+
+        }
+
+        @ApiOperation(value = "View a list of customers")
+        @GetMapping("customers")
+        public ResponseEntity<List<Customer>> getAllCustomers() {
+            List<Customer> list = customerService.getAllCustomers();
+            return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+        }
+
+        @ApiOperation(value = "Add customer")
+        @PostMapping("customer")
+        public ResponseEntity<Void> addCustomer(@RequestBody Customer customer, UriComponentsBuilder builder) {
+            customerService.addCustomer(customer);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(builder.path("/customer/{id}").buildAndExpand(customer.getId()).toUri());
+            return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        }
+
+        @ApiOperation(value = "Update customer")
+        @PutMapping("customer")
+        public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
+            customerService.updateCustomer(customer);
+            return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        }
     }
-
-    @ApiOperation(value = "Search a customer with an NAME")
-    @GetMapping("customers/{name}")
-    public ResponseEntity<List<Customer>> getCustomerByName(@PathVariable("name") String name) {
-        List<Customer>  list = customerService.getCustomerName(name);
-        return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "View a list of customers")
-    @GetMapping("customers")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> list = customerService.getAllCustomers();
-        return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "Add customer")
-    @PostMapping("customer")
-    public ResponseEntity<Void> addCustomer(@RequestBody Customer customer, UriComponentsBuilder builder) {
-        customerService.addCustomer(customer);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/customer/{id}").buildAndExpand(customer.getId()).toUri());
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-    }
-
-    @ApiOperation(value = "Update customer")
-    @PutMapping("customer")
-    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
-        customerService.updateCustomer(customer);
-        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
-    }
-}
