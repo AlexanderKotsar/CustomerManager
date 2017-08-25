435fe2800c5a74618294452cc3a1efe756c7aeaf
Alexander Kotsar <kotsandroid@gmail.com>
2017-08-25 14:35:45 +0300

    Refactoring DB properties ( loading initial database data before the application starts)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id, name, phone, address) VALUES ('1','John','555-11-44','LA'),
+                                                        ('2','Arch','000-55-88','NY'),
+                                                        ('3','Вася','937-99-92','Днепр');
